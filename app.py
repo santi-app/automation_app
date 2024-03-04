9a4065cfdceadbf7e8b3916e63e386e17df00bc9
santi-app <162107607+santi-app@users.noreply.github.com>
2024-03-04 14:31:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import datetime
+import win32com.client
+import shutil
+import os
+import pythoncom
+pythoncom.CoInitialize()
+import tkinter as tk
+from tkinter import filedialog
+import pandas
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import numpy as np
+import pandas as pd
+import time
+from selenium.webdriver.common.action_chains import ActionChains
